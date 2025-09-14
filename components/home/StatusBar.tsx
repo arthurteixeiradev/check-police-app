3aac5996fbde2a901318e1d56de6a629607fc3d7
Regis Filho <filhoregis96@gmail.com>
2025-09-14 18:34:20 -0300

    feat: add navigation from bell icon to notification screen

--- components/home/StatusBar.tsx
+++ components/home/StatusBar.tsx
@@ -1,17 +1,26 @@
-import { Bell, Bolt, CircleUserRound } from "lucide-react-native";
-import { Text, View } from "react-native";
+import { Bell, Bolt, CircleUserRound } from 'lucide-react-native'
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+import { TouchableOpacity } from 'react-native'
 
 export function StatusBar() {
-    return (
-        <View className="h-32 p-6 justify-between">
-            <View className="flex-row justify-between items-center">
-                <CircleUserRound size={25} />
-                <View className="flex-row items-center justify-center gap-6">
-                    <Bell size={25} />
-                    <Bolt size={25} />
-                </View>
-            </View>
-            <Text className="text-2xl font-bold">SD Fulaninho</Text>
+  return (
+    <View className='h-32 p-6 justify-between'>
+      <View className='flex-row justify-between items-center'>
+        <CircleUserRound size={25} />
+        <View className='flex-row items-center justify-center gap-6'>
+          <TouchableOpacity>
+            <Link
+              href='/notification'
+              asChild
+            >
+              <Bell size={25} />
+            </Link>
+          </TouchableOpacity>
+          <Bolt size={25} />
         </View>
-    );
+      </View>
+      <Text className='text-2xl font-bold'>SD Fulaninho</Text>
+    </View>
+  )
 }
